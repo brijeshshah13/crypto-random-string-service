3a072f12d24780e48f67b75ef0f33878cb1d045c
Brijesh Shah <brijeshjain13@gmail.com>
2021-10-15 22:53:47 +0530

    feat: add routes for cockroach db integration

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.17
 
 require (
 	github.com/brijeshshah13/crypto-random-string v0.1.4
+	github.com/cenkalti/backoff/v4 v4.1.1
+	github.com/cockroachdb/cockroach-go/v2 v2.2.1
 	github.com/gin-gonic/gin v1.7.4
 )
 
@@ -15,11 +17,12 @@ require (
 	github.com/golang/protobuf v1.3.3 // indirect
 	github.com/json-iterator/go v1.1.9 // indirect
 	github.com/leodido/go-urn v1.2.0 // indirect
+	github.com/lib/pq v1.10.0 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
 	github.com/ugorji/go/codec v1.1.7 // indirect
-	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
-	golang.org/x/sys v0.0.0-20200116001909-b77594299b42 // indirect
-	gopkg.in/yaml.v2 v2.2.8 // indirect
+	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2 // indirect
+	golang.org/x/sys v0.0.0-20210823070655-63515b42dcdf // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,19 +1,38 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"github.com/brijeshshah13/crypto-random-string"
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/cenkalti/backoff/v4"
+	"github.com/cockroachdb/cockroach-go/v2/crdb"
 )
 
 func main() {
 	r := gin.Default()
+
+	db, err := initStore()
+	if err != nil {
+		log.Fatalf("failed to initialise the store: %s", err)
+	}
+	defer db.Close()
+
+	r.GET("/", func(ctx *gin.Context) {
+		rootHandler(ctx, db)
+	})
+
 	r.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
 	r.GET("/random-string", func(ctx *gin.Context) {
 		// just a practice on how to capture canceled requests
 		select {
@@ -23,16 +42,123 @@ func main() {
 		default:
 		}
 		generator := cryptorandomstring.New()
-		if str, err := generator.WithLength(10).Generate(); err != nil {
-			fmt.Println(err.Error())
-			ctx.JSON(http.StatusInternalServerError, gin.H{
-				"error": err.Error(),
+		if str, err := generator.WithLength(10).WithKind("hex").WithCharacters("abc").Generate(); err != nil {
+			ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+				"err": err.Error(),
 			})
+			return
 		} else {
 			ctx.JSON(http.StatusOK, gin.H{
 				"string": str,
 			})
 		}
 	})
-	r.Run()
+
+	r.POST("/send", func(ctx *gin.Context) {
+		sendHandler(ctx, db)
+	})
+
+	httpPort := os.Getenv("HTTP_PORT")
+	if httpPort == "" {
+		httpPort = "8080"
+	}
+
+	r.Run(":" + httpPort)
+}
+
+type Message struct {
+	Value string `json:"value"`
+}
+
+func initStore() (*sql.DB, error) {
+
+	pgConnString := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=disable",
+		os.Getenv("PGHOST"),
+		os.Getenv("PGPORT"),
+		os.Getenv("PGDATABASE"),
+		os.Getenv("PGUSER"),
+		os.Getenv("PGPASSWORD"),
+	)
+
+	var (
+		db  *sql.DB
+		err error
+	)
+	openDB := func() error {
+		db, err = sql.Open("postgres", pgConnString)
+		return err
+	}
+
+	err = backoff.Retry(openDB, backoff.NewExponentialBackOff())
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err := db.Exec(
+		"CREATE TABLE IF NOT EXISTS message (value STRING PRIMARY KEY)"); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func rootHandler(ctx *gin.Context, db *sql.DB) {
+	r, err := countRecords(db)
+	if err != nil {
+		ctx.String(http.StatusInternalServerError, "%s", err.Error())
+		return
+	}
+	ctx.String(http.StatusOK, "Hello, Docker! (%d)\n", r)
+}
+
+func countRecords(db *sql.DB) (int, error) {
+
+	rows, err := db.Query("SELECT COUNT(*) FROM message")
+	if err != nil {
+		return 0, err
+	}
+	defer rows.Close()
+
+	count := 0
+	for rows.Next() {
+		if err := rows.Scan(&count); err != nil {
+			return 0, err
+		}
+		rows.Close()
+	}
+
+	return count, nil
+}
+
+func sendHandler(ctx *gin.Context, db *sql.DB) {
+
+	m := &Message{}
+
+	if err := ctx.Bind(m); err != nil {
+		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"err": err.Error(),
+		})
+		return
+	}
+
+	err := crdb.ExecuteTx(context.Background(), db, nil,
+		func(tx *sql.Tx) error {
+			_, err := tx.Exec(
+				"INSERT INTO message (value) VALUES ($1) ON CONFLICT (value) DO UPDATE SET value = excluded.value",
+				m.Value,
+			)
+			if err != nil {
+				return ctx.AbortWithError(http.StatusInternalServerError, err)
+			}
+			return nil
+		})
+
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"err": err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, m)
 }
